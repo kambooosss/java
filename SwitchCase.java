e6f2f87fac263558bb3c2cc2b0986b4084d12905
kambooosss <kamaleshkumar22112002@gmail.com>
2023-05-25 11:45:13 +0530

    different switch case form

---
+++ SwitchCase.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        // switch(day)
+        // {
+        //     case 1, 2,3,4,5 -> System.out.println("Weekday");
+        //     case 6,7 -> System.out.println("weedend");
+        // }
+
+        //THIS KIND OF SWITCH CASE IS AVAILABLE ONLY IN JAVA 14
+
+        //WHICH CAN BE IMPLEMENTED BY 
+
+        switch(day)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            System.out.println("weeddays");;
+            break;
+            case 6:
+            case 7:
+            System.out.println("weekday");
+            break;
+        }
+            
+
+    }
+}
